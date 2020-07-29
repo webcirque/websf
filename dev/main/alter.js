0214f42f8f07974209f10765720c8e53c730418d
Ediel Chedlyramber <15964383+EdChdX@users.noreply.github.com>
2020-07-29 19:16:46 +0800

    Update alter.js

--- dev/main/alter.js
+++ dev/main/alter.js
@@ -1,91 +1,185 @@
 "use strict";
 
+// Ahead
+var walterna = [];
+if (!Array.prototype.indexOf) {
+	walterna.push("noIndexOf");
+};
+// If a number has...
+Number.prototype.inside = function (maxRange) {
+	return (this % maxRange + maxRange) % maxRange;
+};
+Number.prototype.within = function (min, max) {
+	var range = max - min;
+	if (range > 0) {
+		return ((this - min).inside(range) + min);
+	} else {
+		throw(new RangeError("Maximum value cannot be lower than minimum value"));
+	};
+};
+// Primitive compatibility layer for array
+Array.from = Array.from || function (target) {
+	var ans = [];
+	if (target.length) {
+		if (target.length >= 0) {
+			for (var pt = 0; pt < target.length; pt ++) {
+				ans.push(target[pt]);
+			};
+		} else {
+			throw Error("Illegal length");
+		};
+	};
+	return ans;
+};
+Array.prototype.filter = Array.prototype.indexOf || function (func) {
+	var ans = [];
+	if (func) {
+		if (func.constructor == Function) {
+			for (var pt = 0; pt < this.length; pt ++) {
+				if (func(this[pt])) {
+					ans.push(this[pt]);
+				};
+			};
+		};
+	};
+	return ans;
+};
+Array.prototype.forEach = Array.prototype.forEach || function (defFunc) {
+	for (var ptc = 0; ptc < this.length; ptc ++) {
+		defFunc(this[ptc], ptc, this);
+	};
+};
+Array.prototype.indexOf = Array.prototype.indexOf || function (del) {
+	var ans = -1;
+	if (del) {
+		for (var pt = 0; pt < this.length; pt ++) {
+			if (this[pt] == del) {
+				ans = pt;
+				break;
+			};
+		};
+	};
+	return ans;
+};
+Array.prototype.lastIndexOf = Array.prototype.lastIndexOf || function (del) {
+	var ans = -1;
+	if (del) {
+		for (var pt = 0; pt < this.length; pt ++) {
+			if (this[pt] == del) {
+				ans = pt;
+			};
+		};
+	};
+	return ans;
+};
 // If an array has...
-Array.prototype.withCount = function () {
-    var act = 0;
-    for (var pt = 0; pt < arguments.length; pt ++) {
-        if (this.indexOf(arguments[pt]) != -1) {
-            act ++;
-        };
-    };
-    return act;
+Array.prototype.reversed = function () {
+	return this.slice().reverse();
 };
-Array.prototype.withAny = function () {
-    var ans = false;
-    for (var pt = 0; pt < arguments.length; pt ++) {
-        if (this.indexOf(arguments[pt]) != -1) {
-            ans = true;
-        };
-        if (ans == true) {
-            continue;
-        };
-    };
-    return ans;
+Array.prototype.withCount = function (args) {
+	var act = 0;
+	for (var pt = 0; pt < args.length; pt ++) {
+		if (this.indexOf(args[pt]) != -1) {
+			act ++;
+		};
+	};
+	return act;
 };
-Array.prototype.withAll = function () {
-    var act = 0;
-    for (var pt = 0; pt < arguments.length; pt ++) {
-        if (this.indexOf(arguments[pt]) != -1) {
-            act ++;
-        };
-    };
-    return (act == arguments.length);
+Array.prototype.withAny = function (args) {
+	return (this.withCount(args) > 0);
+};
+Array.prototype.withAll = function (args) {
+	return (this.withCount(args) == args.length);
+};
+Array.prototype.withCountd = function () {
+	return this.withCount(arguments);
+};
+Array.prototype.withAnyd = function () {
+	return this.withAny(arguments);
+};
+Array.prototype.withAlld = function () {
+	return this.withAll(arguments);
 };
 Array.prototype.matchAny = function (args) {
-    var a1 = this, a2 = args, ans = false;
-    if (a1.length < a2.length) {
-        var a3 = a1;
-        a1 = a2;
-        a2 = a3;
-    };
-    for (var pt = 0; pt1 < a2.length; pt ++) {
-        if (a1.indexOf(a2[pt]) != -1) {
-            ans = true;
-        };
-    };
-    return ans;
+	var a1 = this, a2 = args, ans = false;
+	if (a1.length < a2.length) {
+		var a3 = a1;
+		a1 = a2;
+		a2 = a3;
+	};
+	for (var pt = 0; pt1 < a2.length; pt ++) {
+		if (a1.indexOf(a2[pt]) != -1) {
+			ans = true;
+		};
+	};
+	return ans;
 };
 
 // Batch type comparison, one array-based, one argument-based
 try {
-    var Compare = function () {
-        this.type = function (dType, args) {
-            var count = 0;
-            for (var pt = 0; pt < args.length; pt ++) {
-                if (!(!(args[pt]))) {
-                    if (args[pt].constructor == dType) {
-                        count ++;
-                    };
-                };
-            };
-            return count;
-        };
-        this.able = function (args) {
-            var count = 0;
-            for (var pt = 0; pt < args.length; pt ++) {
-                if (!(!(args[pt]))) {
-                    count ++;
-                };
-            };
-            return count;
-        };
-    };
-    Compare = new Compare();
+	var Compare = function () {
+		this.type = function (dType, args) {
+			var count = 0;
+			Array.from(args).forEach(function (e) {
+				if (!!e) {
+					if (e.constructor == dType) {
+						count ++;
+					};
+				};
+			});
+			return count;
+		};
+		this.able = function (args) {
+			var count = 0;
+			Array.from(args).forEach(function (e) {
+				if (e != null && e != undefined) {
+					count ++;
+				};
+			});
+			return count;
+		};
+	};
+	Compare = new Compare();
 } catch (err) {};
 try {
-    var Compard = function () {
-        this.type = function () {
-            var dType = arguments[0];
-            var args = Array.from(arguments).slice(1, arguments.length - 1);
-            return Compare.type(dType, args);
-        };
-        this.able = function () {
-            return Compare.able(arguments);
-        };
-    };
-    Compard = new Compard();
+	var Compard = function () {
+		this.type = function () {
+			var dType = arguments[0];
+			var args = Array.from(arguments).slice(1, arguments.length - 1);
+			return Compare.type(dType, args);
+		};
+		this.able = function () {
+			return Compare.able(arguments);
+		};
+	};
+	Compard = new Compard();
 } catch (err) {};
-
+// If a string has...
+String.prototype.withCount = function (args) {
+	var count = 0, copied = this.slice();
+	Array.from(args).forEach(function (e) {
+		if (copied.indexOf(e) != -1) {
+			count ++;
+		};
+	});
+	return count;
+};
+String.prototype.withAny = function (args) {
+	return (this.withCount(args) > 0);
+};
+String.prototype.withAll = function (args) {
+	return (this.withCount(args) == args.length);
+};
+String.prototype.withCountd = function () {
+	return this.withCount(arguments);
+};
+String.prototype.withAnyd = function () {
+	return (this.withCount(arguments) > 0);
+};
+String.prototype.withAlld = function () {
+	return (this.withCount(arguments) == arguments.length);
+};
+String.prototype.formText = function (map) {};
 /* function wAlter (text, map) {
 	let wtAr = Array.from(text);
 	let wlist = [];
